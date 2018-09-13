90d06b3d6eceeb5204a933e494ca1d7f581f962d
shahidhn <shahidhn.nowshad@gmail.com>
2018-09-13 16:08:06 -0500

    So close yet so far

--- hw1/q6/Tournament/TournamentLock.java
+++ hw1/q6/Tournament/TournamentLock.java
@@ -1,72 +1,76 @@
-package q6.Tournament;
+/* Shahid's implementation */
 
-import java.util.Arrays;
+//package q6.Tournament;
+//
+//import java.util.Arrays;
+//
+//public class TournamentLock implements Lock {
+//	int n, levels;
+//	volatile PLock[] locks;
+//	volatile int[][] pIDs;
+//
+//	public static double log2(int n){
+//		return Math.log(n)/Math.log(2);
+//	}
+//	
+//	public TournamentLock(int numThreads){
+//		this.n = numThreads;
+//		this.levels = (int) Math.ceil(log2(n)); 
+//		this.locks = new PLock[n];
+//		this.pIDs = new int[numThreads][levels];
+//		Arrays.fill(locks, new PLock());
+//		for (int i = 0; i < numThreads; i ++){
+//			for (int j = 0; j < levels; j ++){
+//				pIDs[i][j] = -1;
+//			}
+//		}
+//	}
+//	
+//	public void lock(int i) {
+//		int nodeID = i + n;
+//		
+//		for (int j = 0; j < levels; j ++){
+//			pIDs[i][j] = nodeID % 2;
+//			nodeID = nodeID / 2;
+//			System.out.println("Process " + i + " requesting lock " + nodeID + " with pID " + pIDs[i][j]);
+//			locks[nodeID].requestCS(pIDs[i][j]);;
+//		}
+//	}
+//	
+//	public void unlock(int i) {
+//		int nodeID = 1;
+//		for (int j = levels-1; j >= 0; j --){
+//			System.out.println("Process " + i + " releasing lock " + nodeID + " with pID " + pIDs[i][j]);
+//			locks[nodeID].releaseCS(pIDs[i][j]);
+//			nodeID = 2 * nodeID + pIDs[i][j];
+//		}
+//	}
+//	
+//	class PLock {
+//		volatile boolean[] wantCS = new boolean[2];
+//		volatile int turn;
+//		
+//		public PLock(){
+//			Arrays.fill(this.wantCS, false);
+//			turn = 1;
+//		}
+//		
+//		public void requestCS(int i){
+//			int j = 1 - i;
+//			wantCS[i] = true;
+//			turn = j;
+//			while (wantCS[j] && turn == j);
+//		}
+//		
+//		public void releaseCS(int i){
+//			wantCS[i] = false;
+//		}
+//		
+//	}
+//	
+//}
 
-public class TournamentLock implements Lock {
-	int n, levels;
-	volatile PLock[] locks;
-	volatile int[][] pIDs;
-
-	public static double log2(int n){
-		return Math.log(n)/Math.log(2);
-	}
-	
-	public TournamentLock(int numThreads){
-		this.n = numThreads;
-		this.levels = (int) Math.ceil(log2(n)); 
-		this.locks = new PLock[n];
-		this.pIDs = new int[numThreads][levels];
-		Arrays.fill(locks, new PLock());
-		for (int i = 0; i < numThreads; i ++){
-			for (int j = 0; j < levels; j ++){
-				pIDs[i][j] = -1;
-			}
-		}
-	}
-	
-	public void lock(int i) {
-		int nodeID = i + n;
-		
-		for (int j = 0; j < levels; j ++){
-			pIDs[i][j] = nodeID % 2;
-			nodeID = nodeID / 2;
-			System.out.println("Process " + i + " requesting lock " + nodeID + " with pID " + pIDs[i][j]);
-			locks[nodeID].requestCS(pIDs[i][j]);;
-		}
-	}
-	
-	public void unlock(int i) {
-		int nodeID = 1;
-		for (int j = levels-1; j >= 0; j --){
-			System.out.println("Process " + i + " releasing lock " + nodeID + " with pID " + pIDs[i][j]);
-			locks[nodeID].releaseCS(pIDs[i][j]);
-			nodeID = 2 * nodeID + pIDs[i][j];
-		}
-	}
-	
-	class PLock {
-		volatile boolean[] wantCS = new boolean[2];
-		volatile int turn;
-		
-		public PLock(){
-			Arrays.fill(this.wantCS, false);
-			turn = 1;
-		}
-		
-		public void requestCS(int i){
-			int j = 1 - i;
-			wantCS[i] = true;
-			turn = j;
-			while (wantCS[j] && turn == j);
-		}
-		
-		public void releaseCS(int i){
-			wantCS[i] = false;
-		}
-		
-	}
-	
-}
+/* Jacob's implementation */
 
 //package q6.Tournament;
 //
@@ -122,58 +126,60 @@ public class TournamentLock implements Lock {
 //	
 //}
 
-//package q6.Tournament;
-//
-//import java.util.Arrays;
-//
-//public class TournamentLock implements Lock {
-//	int q, n, p, z, levels;
-//	
-//	volatile int[] gate;
-//	volatile boolean[] flag;
-//	volatile int[] last;
-//	
-//	public static double log2(int n){
-//		return Math.log(n)/Math.log(2);
-//	}
-//	
-//	public TournamentLock(int numThreads){
-//		this.q = (int) Math.pow(2, (int) Math.ceil(log2(numThreads)));
-//		this.n = q + q - 1;
-//		this.p = q - 1;
-//		this.z = numThreads;
-//		this.levels = (int) Math.ceil((log2(z)));
-//		
-//		this.gate = new int[numThreads * z];
-//		this.flag = new boolean[n];
-//		this.last = new int[p];
-//		
-//		Arrays.fill(this.gate, -1);
-//		Arrays.fill(this.flag, false);
-//	}
-//	public void lock(int i) {
-//		i ++;
-//		int ipid = p + i - 1;
-//		
-//		for (int j = 0; j < levels; j ++){
-//			flag[ipid] = true;
-//			last[(ipid-1)/2] = i;
-//			
-//			if (ipid % 2 == 0){
-//				while (flag[ipid-1] && last[(ipid-1)/2] == i);
-//			} else {
-//				while (flag[ipid+1] && last[(ipid-1)/2] == i);
-//			}
-//			
-//			gate[(i-1)*z+j] = ipid;
-//			ipid = (ipid-1)/2;
-//		}
-//	}
-//	
-//	public void unlock(int i) {
-//		for (int j = 0; j < levels; j ++){
-//			flag[gate[i*z+j]] = false;
-//		}
-//	}
-//	
-//}
+/* Taken from https://github.com/shriroopjoshi/mutual-exclusion/blob/master/tournament.cpp */
+
+package q6.Tournament;
+
+import java.util.Arrays;
+
+public class TournamentLock implements Lock {
+	int q, n, p, z, levels;
+	
+	volatile int[] gate;
+	volatile boolean[] flag;
+	volatile int[] last;
+	
+	public static double log2(int n){
+		return Math.log(n)/Math.log(2);
+	}
+	
+	public TournamentLock(int numThreads){
+		this.q = (int) Math.pow(2, (int) Math.ceil(log2(numThreads)));
+		this.n = q + q - 1;
+		this.p = q - 1;
+		this.z = numThreads;
+		this.levels = (int) Math.ceil((log2(z)));
+		
+		this.gate = new int[numThreads * z];
+		this.flag = new boolean[n];
+		this.last = new int[p];
+		
+		Arrays.fill(this.gate, -1);
+		Arrays.fill(this.flag, false);
+	}
+	public void lock(int i) {
+		i ++;
+		int ipid = p + i - 1;
+		
+		for (int j = 0; j < levels; j ++){
+			flag[ipid] = true;
+			last[(ipid-1)/2] = i;
+			
+			if (ipid % 2 == 0){
+				while (flag[ipid-1] && last[(ipid-1)/2] == i);
+			} else {
+				while (flag[ipid+1] && last[(ipid-1)/2] == i);
+			}
+			
+			gate[(i-1)*z+j] = ipid;
+			ipid = (ipid-1)/2;
+		}
+	}
+	
+	public void unlock(int i) {
+		for (int j = 0; j < levels; j ++){
+			flag[gate[i*z+j]] = false;
+		}
+	}
+	
+}
