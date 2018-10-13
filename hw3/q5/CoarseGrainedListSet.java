d6972ef14b8cda27ba2e19bfeb7c553801b50f7d
shahidhn <shahidhn.nowshad@gmail.com>
2018-10-13 13:43:57 -0500

    5a done

--- hw3/q5/CoarseGrainedListSet.java
+++ hw3/q5/CoarseGrainedListSet.java
@@ -1,25 +1,102 @@
 package q5;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class CoarseGrainedListSet implements ListSet {
 // you are free to add members
+	private final ReentrantLock lock;
+	Node head;
 	
   public CoarseGrainedListSet() {
-	// implement your constructor here	  
+	this.lock = new ReentrantLock();
+	this.head = new Node(null);
   }
   
   public boolean add(int value) {
-	// implement your add method here	
-    return false;
+	lock.lock();
+	
+	// Insert at head
+	if (head.value == null){
+		head.value = value;
+		
+		lock.unlock();
+		return true;
+	}
+	
+	Node current = head;
+	Node prev = current;
+	
+	while (current.next != null && current.value < value){
+		prev = current;
+		current = current.next;
+	}
+	// Exit while loop if current.next == null OR current.value >= value
+	
+	if (current.value == value){
+		lock.unlock();
+		return false;
+	} else if (current.value > value){
+		Node n = new Node(value);
+		prev.next = n;
+		n.next = current;
+		
+		lock.unlock();
+		return true;
+	} else { // current.value < value AND current.next == null
+		Node n = new Node(value);
+		current.next = n;
+		
+		lock.unlock();
+		return true;
+	}
+	
   }
   
   public boolean remove(int value) {
-	// implement your remove method here	
-    return false;
+	lock.lock();
+	
+	// Check head
+	if (head.value == null){
+		lock.unlock();
+		return false;
+	}
+	
+	Node current = head;
+	Node prev = current;
+	
+	while (current.next != null && current.value < value){
+		prev = current;
+		current = current.next;
+	}
+	// Exit while loop if current.next == null OR current.value >= value
+	
+	if (current.value == value){
+		prev.next = current.next;
+		
+		lock.unlock();
+		return true;
+	} else { // (current.value > value) || (current.value < value && current.next == null)
+		lock.unlock();
+		return false;
+	}
+    
   }
   
   public boolean contains(int value) {
-	// implement your contains method here	
-    return false;
+	lock.lock();
+	if (head.value == null){
+		lock.unlock();
+		return false;
+	}
+	
+	Node current = head;
+	while (current.value < value && current.next != null){
+		current = current.next;
+	}
+	
+	lock.unlock();
+	return current.value == value;
+	
   }
   
   protected class Node {
@@ -37,6 +114,21 @@ public class CoarseGrainedListSet implements ListSet {
   check simpleTest for more info
   */
   public String toString() {
-    return "";
+    String s = "";
+    lock.lock();
+    if (head.value == null){
+    	lock.unlock();
+    	return s;
+    }
+    
+    Node current = head;
+    while (current != null){
+    	s = s + current.value + ",";
+    	current = current.next;
+    }
+    
+    lock.unlock();
+    return s;
+    
   }
 }
